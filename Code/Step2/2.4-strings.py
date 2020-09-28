d5f633062c43ab1b6738e1bed6a79b957e55d97d
Alex-Rider <amuzhikov@gmail.com>
2020-09-28 14:38:11 +0300

    worked on laptop.

--- Code/Step2/2.4-strings.py
+++ Code/Step2/2.4-strings.py
@@ -1,4 +1,16 @@
-chain = input().lower()
-Gs, Cs = chain.count("g"), chain.count("c")
-print((Gs + Cs) / len(chain) * 100)
+# chain = input().lower()
+# Gs, Cs = chain.count("g"), chain.count("c")
+# print((Gs + Cs) / len(chain) * 100)
 
+
+chain = input().lower()
+res = chain[0]
+cnt = 0
+for sym in chain:
+    if sym == res[-1]:
+        cnt += 1
+    else:
+        res += str(cnt)
+        res += sym
+        cnt = 1
+print(res)
